d224640ff50b4f7394dec00d68f29dce50557fc2
Ad1s <y.bryak@gmail.com>
2025-11-24 17:31:16 +0100

    Update build script dependencies
    Added GSON and Lombok libraries for serialization and data management respectively

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "com.ajibprojects"
+group = "com.byt"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,14 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.code.gson:gson:2.13.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
