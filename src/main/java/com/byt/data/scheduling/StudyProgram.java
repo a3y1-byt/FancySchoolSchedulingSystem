769ecdb1f8f66bfa4766247856a2d95ef08230d7
Ad1s <y.bryak@gmail.com>
2026-01-07 23:21:27 +0100

    Implement reverse methods for left classes
    Specialization, StudyProgram, Subject and Semester got their association methods and getters.

--- src/main/java/com/byt/data/scheduling/StudyProgram.java
+++ src/main/java/com/byt/data/scheduling/StudyProgram.java
@@ -1,16 +1,23 @@
 package com.byt.data.scheduling;
 
 import com.byt.enums.scheduling.StudyProgramLevel;
-import lombok.Builder;
-import lombok.Data;
+import com.byt.validation.scheduling.Validator;
+import com.byt.validation.user_system.UserValidator;
+import lombok.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Builder
 public class StudyProgram {
-    String name;
-    StudyProgramLevel level;
-    List<Specialization> specializations;
+    private String name;
+    private StudyProgramLevel level;
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Set<Specialization> specializations = new HashSet<>();
 
     public static StudyProgram copy(StudyProgram program) {
         return StudyProgram.builder()
@@ -19,4 +26,27 @@ public class StudyProgram {
                 .build();
     }
 
+    public Set<Specialization> getSpecializations() {
+        return new HashSet<>(specializations);
+    }
+
+    public void addSpecialization(Specialization specialization) {
+        Validator.validateSpecialization(specialization);
+
+        if (specializations.contains(specialization))
+            return;
+
+        specializations.add(specialization);
+        specialization.addStudyProgram(this);
+    }
+
+    public void removeSpecialization(Specialization specialization) {
+        Validator.validateSpecialization(specialization);
+
+        if (!specializations.contains(specialization))
+            return;
+
+        specializations.remove(specialization);
+        specialization.removeStudyProgram(this);
+    }
 }
--- src/main/java/com/byt/data/scheduling/Subject.java
+++ src/main/java/com/byt/data/scheduling/Subject.java
@@ -1,19 +1,31 @@
 package com.byt.data.scheduling;
 
 import com.byt.enums.scheduling.SubjectType;
-import lombok.Builder;
-import lombok.Data;
+import com.byt.validation.scheduling.Validator;
+import lombok.*;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Builder
 public class Subject {
     public static final int HOURS_PER_ECTS = 25;
-    String name;
-    int hours;
-    List<SubjectType> types;
-    List<Lesson> lessons;
+
+    private String name;
+    private int hours;
+
+    private Set<SubjectType> types = new HashSet<>();
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Set<Lesson> lessons = new HashSet<>();
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Set<Specialization> specializations = new HashSet<>();
 
     public int getEcts() {
         return hours / HOURS_PER_ECTS;
@@ -23,9 +35,57 @@ public class Subject {
         return Subject.builder()
                 .name(subject.getName())
                 .types(subject.getTypes() != null
-                        ? new ArrayList<>(subject.getTypes())
-                        : new ArrayList<>())
+                        ? new HashSet<>(subject.getTypes())
+                        : new HashSet<>())
                 .hours(subject.getHours())
                 .build();
+    }
+
+    public Set<Lesson> getLessons() {
+        return new HashSet<>(lessons);
+    }
+
+    public Set<Specialization> getSpecializations() {
+        return new HashSet<>(specializations);
+    }
+
+    public void addSpecialization(Specialization specialization) {
+        Validator.validateSpecialization(specialization);
+
+        if (specializations.contains(specialization))
+            return;
+
+        specializations.add(specialization);
+        specialization.addSubject(this);
+    }
+
+    public void removeSpecialization(Specialization specialization) {
+        Validator.validateSpecialization(specialization);
+
+        if (!specializations.contains(specialization))
+            return;
+
+        specializations.remove(specialization);
+        specialization.removeSubject(this);
+    }
+
+    public void addLesson(Lesson lesson) {
+        Validator.validateLesson(lesson);
+
+        if (lessons.contains(lesson))
+            return;
+
+        lessons.add(lesson);
+        lesson.addSubject(this);
+    }
+
+    public void removeLesson(Lesson lesson) {
+        Validator.validateLesson(lesson);
+
+        if (!lessons.contains(lesson))
+            return;
+
+        lessons.remove(lesson);
+        lesson.removeSubject(this);
     }
 }
